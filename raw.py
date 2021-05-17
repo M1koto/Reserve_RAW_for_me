d60b831adc41fcc55f5372bdb27a8e3b1c8fcc88
Kenny Liao <yenming891109@berkeley.edu>
2021-05-17 15:49:26 +0800

    no space to try la

--- raw.py
+++ raw.py
@@ -1,13 +1,29 @@
 import urllib.request as u
+import sys, requests
 
 email = 'kennytowerofsaviors@gmail.com'
 phone = '0935276755'
-num__ppl = 2
-web = urllib.request.urlopen("https://www.raw.com.tw/RawBooking.aspx?Language=en")
+num_ppl = 2
+web = u.urlopen("https://www.raw.com.tw/RawBooking.aspx?Language=en")
 date = '2000-11-09'
 
-query_page = "http://api.eztable.com/v3/hotpot/quota/{0}?restaurant_id=14039&people={1}".format(date, num__ppl)
 assert "200" == str(web.getcode())
 
+code = ""
 
+if __name__ == "__main__":
+   num_ppl = sys.argv[1]
+   date = sys.argv[2]
+   #format query
+   query_page = "http://api.eztable.com/v3/hotpot/quota/{0}?restaurant_id=14039&people={1}".format(date, num_ppl)
+   #send post request to eztable
+
+
+   #done sending post request to eztable
+   #start entering phone code
+   print('Enter phone code')
+   code = input()
+   data = {'tel': "+886935276755", 'country_code': "tw", 'otp': code}
+   url = 'https://api.eztable.com/v3/auth/verification-otp-code/?locale=en_US'
+   response = requests.post(url, data = data)
 
